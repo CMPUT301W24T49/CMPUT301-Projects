06b8fb77363a570976a203cc7ff85cca8e88d751
aimansaif <asaif@ualberta.ca>
2024-03-07 14:17:18 -0700

    Admin Settings without admin profile details functionality

---
+++ app/src/main/java/com/example/qr/activities/AdminSettingsFragment.java
@@ -0,0 +1,53 @@
+package com.example.qr.activities;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.qr.R;
+
+public class AdminSettingsFragment extends Fragment {
+
+    public AdminSettingsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_admin_settings, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        View profileButton = view.findViewById(R.id.profile_button);
+        profileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle profile button click
+            }
+        });
+
+        View logoutButton = view.findViewById(R.id.logout_button);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle logout button click
+                Fragment homeFrag = new HomeFragment();
+                if (getActivity() != null) {
+                    getActivity().getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.fragment_container, homeFrag)
+                            .addToBackStack(null)  // Optional: Add transaction to back stack
+                            .commit();
+                }
+            }
+        });
+    }
+
+}
