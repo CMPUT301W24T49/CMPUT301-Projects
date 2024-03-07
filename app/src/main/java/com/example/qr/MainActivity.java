ec6e27add8fb14c6a450dfc4528b21f7f18f6161
basharharash <90409951+basharharash@users.noreply.github.com>
2024-03-06 22:56:36 -0700

    added some comments instructions for how to change the starting main menu

--- app/src/main/java/com/example/qr/MainActivity.java
+++ app/src/main/java/com/example/qr/MainActivity.java
@@ -9,6 +9,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // for now, im setting the main menu to the attendee main menu
+        // feel free to change it to the organizer main menu if you want to preview your UI
+        // feel free to change it to the admin main menu if you want to preview your UI
+        // example: setContentView(R.layout.organizer_main_menu);
+        // example: setContentView(R.layout.admin_main_menu);
         setContentView(R.layout.attendee_main_menu);
     }
 }
