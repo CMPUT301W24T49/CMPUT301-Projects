1f76e3a58128fad8b1b011aaafe167a225fa68ac
Jesse Zheng <jessezheng19@gmail.com>
2024-03-07 20:51:04 -0700

    organizer settings template

--- app/src/main/java/com/example/qr/activities/OrganizerSettingsFragment.java
+++ app/src/main/java/com/example/qr/activities/OrganizerSettingsFragment.java
@@ -3,4 +3,7 @@ package com.example.qr.activities;
 import androidx.fragment.app.Fragment;
 
 public class OrganizerSettingsFragment extends Fragment {
+    public OrganizerSettingsFragment() {
+        // Required empty public constructor
+    }
 }
