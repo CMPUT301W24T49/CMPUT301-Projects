d6e2b4a209437cac78eac4aa94374362b753fbfb
nachkt <reachrainanachiket@gmail.com>
2024-03-07 19:45:22 -0700

    stuff

--- app/src/main/java/com/example/qr/utils/FirebaseUtil.java
+++ app/src/main/java/com/example/qr/utils/FirebaseUtil.java
@@ -1,5 +1,7 @@
 package com.example.qr.utils;
 
+import android.net.Uri;
+
 import com.example.qr.models.CheckIn;
 import com.example.qr.models.Event;
 import com.example.qr.models.Notification;
@@ -8,10 +10,15 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
 
 public class FirebaseUtil {
 
     private static final FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private static final FirebaseStorage storage = FirebaseStorage.getInstance();
+
 
     /**
      * Adds a new event to the Firestore database.
@@ -64,6 +71,28 @@ public class FirebaseUtil {
                 .addOnSuccessListener(onSuccessListener)
                 .addOnFailureListener(onFailureListener);
     }
+
+    /**
+     * Uploads an image to Firebase Storage and retrieves its download URL.
+     *
+     * @param imageUri The Uri of the image to upload.
+     * @param path The storage path where the image will be saved.
+     * @param onSuccessListener Callback for successful upload, returns the image download URL.
+     * @param onFailureListener Callback for upload failure.
+     */
+    public static void uploadImageAndGetUrl(Uri imageUri, String path, OnSuccessListener<Uri> onSuccessListener, OnFailureListener onFailureListener) {
+        StorageReference storageRef = storage.getReference();
+        StorageReference imageRef = storageRef.child(path);
+
+        imageRef.putFile(imageUri).continueWithTask(task -> {
+            if (!task.isSuccessful() && task.getException() != null) {
+                throw task.getException();
+            }
+            // Continue with the task to get the download URL
+            return imageRef.getDownloadUrl();
+        }).addOnSuccessListener(onSuccessListener).addOnFailureListener(onFailureListener);
+    }
+
 
     // You can add more methods here for updating and deleting documents, querying collections, etc.
 }
